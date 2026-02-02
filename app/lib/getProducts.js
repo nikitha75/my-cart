d5da0977f3a9b9152a23b91f243f8fa366670f7f
nikitha75 <nikithanikki474@gmail.com>
2026-02-02 19:42:57 +0530

    Fix bug

--- app/lib/getProducts.js
+++ app/lib/getProducts.js
@@ -1,5 +1,7 @@
 export const getProducts = async () => {
-  const res = await fetch("https://fakestoreapi.com/products");
+  const res = await fetch("https://fakestoreapi.com/products", {
+    next: { revalidate: 60 },
+  });
   if (!res.ok) throw new Error("Failed to fetch data");
   return res.json();
 };
